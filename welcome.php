9e37925a2d3a2c79160086bfaf36e56ee5e302a9
ssari6527 <ssari65274@gmail.com>
2026-07-29 13:42:31 +0700

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Script PHP Sederhana
+ * Deskripsi: Menampilkan pesan ucapan selamat datang.
+ */
+
+// Menyimpan variabel nama
+$nama = "Pengunjung";
+
+// Menampilkan pesan di layar
+echo "Halo, selamat datang " . $nama . "!";
+?>
